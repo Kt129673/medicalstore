d8da7657a8f03b23d16d5aaa8b717104aab0c3f9
Kt129673 <99062016+Kt129673@users.noreply.github.com>
2026-03-06 14:16:17 +0530

    feat: initialize Spring Boot application with timezone, async, and scheduling setup.

--- src/main/java/com/medicalstore/MedicalstoreApplication.java
+++ src/main/java/com/medicalstore/MedicalstoreApplication.java
@@ -2,10 +2,11 @@ package com.medicalstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { KafkaAutoConfiguration.class })
 @EnableAsync
 @EnableScheduling
 public class MedicalstoreApplication {
